2f34c80e578172002640936acd7a87c10a1a2f4a
Pavel Shevchuk <stlwrt@gmail.com>
2026-05-14 20:11:59 +0300

    Enabled Ruff and reformatted the main python file

--- phoronix-rss-augmented.py
+++ phoronix-rss-augmented.py
@@ -1,28 +1,29 @@
-import sys
-from bs4 import BeautifulSoup
 import hashlib
+import logging
+import logging.config
 import math
 import os
 import pathlib
-import humanize
-from requests import Session
-from urllib3.util.retry import Retry
-from requests.adapters import HTTPAdapter
-import time
-from lxml.etree import CDATA, parse, ElementTree, Element
-from glob import glob
-import sentry_sdk
 import re
-import logging
-import logging.config
+import sys
+import time
+from glob import glob
+
+import humanize
 import newrelic.agent
+import sentry_sdk
+from bs4 import BeautifulSoup
+from lxml.etree import CDATA, Element, ElementTree, parse
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 # Source RSS URL
-WEBSITE_ROOT_URL = 'https://www.phoronix.com'
+WEBSITE_ROOT_URL = "https://www.phoronix.com"
 SOURCE_RSS_URL = f"{WEBSITE_ROOT_URL}/rss.php"
 
 # HTTP request properties
-HTTP_REQUEST_INTERVAL = 15 # also used as backoff_factor when retrying failed requests
+HTTP_REQUEST_INTERVAL = 15  # also used as backoff_factor when retrying failed requests
 HTTP_RETRY_ATTEMPT_COUNT = 5
 
 # Define file paths
@@ -30,13 +31,14 @@ PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
 
 # Define cache properties
 CACHE_ROOT = os.path.join(PROJECT_ROOT, "cache")
-CACHE_SOURCE_RSS_FILE_PATH = os.path.join(CACHE_ROOT, 'source_rss.xml')
-CACHE_SOURCE_TTL = 55 # minutes
-CACHE_ITEM_TTL = 24 # hours
+CACHE_SOURCE_RSS_FILE_PATH = os.path.join(CACHE_ROOT, "source_rss.xml")
+CACHE_SOURCE_TTL = 55  # minutes
+CACHE_ITEM_TTL = 24  # hours
 
 # Define output properties
 OUTPUT_ROOT = os.path.join(PROJECT_ROOT, "output")
-OUTPUT_RSS_FILE_PATH = os.path.join(OUTPUT_ROOT, 'phoronix-rss-augmented.xml')
+OUTPUT_RSS_FILE_PATH = os.path.join(OUTPUT_ROOT, "phoronix-rss-augmented.xml")
+
 
 def report_failure_and_exit():
     if betterstack_heartbeat_url:
@@ -46,6 +48,7 @@ def report_failure_and_exit():
             logger.error("Failed!")
         logger.info(f"Response: [{response.status_code}]")
     sys.exit(1)
+
 
 def fetch_and_cache(url, cache_path):
     logger.info(f"Fetching fresh copy of {url}")
@@ -58,9 +61,10 @@ def fetch_and_cache(url, cache_path):
         logger.error("\nResponse.text:")
         logger.error(response.text)
         report_failure_and_exit()
-    with open(cache_path, "w", encoding='utf-8') as f:
+    with open(cache_path, "w", encoding="utf-8") as f:
         f.write(response.text)
     return response.text
+
 
 # Init Sentry before doing anything that might raise exception
 try:
@@ -76,7 +80,9 @@ except OSError:
 # Attempt to load Better Stack heartbeat token
 betterstack_heartbeat_url = None
 try:
-    betterstack_heartbeat_url = pathlib.Path(os.path.join(PROJECT_ROOT, "heartbeat.url")).read_text().strip()
+    betterstack_heartbeat_url = (
+        pathlib.Path(os.path.join(PROJECT_ROOT, "heartbeat.url")).read_text().strip()
+    )
 except OSError:
     pass
 
@@ -104,10 +110,12 @@ except OSError:
 # to avoid crashing on monthly DNS resolution failures
 # https://stackoverflow.com/questions/23013220/max-retries-exceeded-with-url-in-requests
 requests = Session()
-request_retry_config = Retry(total=HTTP_RETRY_ATTEMPT_COUNT, backoff_factor=HTTP_REQUEST_INTERVAL)
+request_retry_config = Retry(
+    total=HTTP_RETRY_ATTEMPT_COUNT, backoff_factor=HTTP_REQUEST_INTERVAL
+)
 http_adapter = HTTPAdapter(max_retries=request_retry_config)
-requests.mount('http://', http_adapter)
-requests.mount('https://', http_adapter)
+requests.mount("http://", http_adapter)
+requests.mount("https://", http_adapter)
 
 current_timestamp = time.time()
 
@@ -116,8 +124,12 @@ if not os.path.isfile(CACHE_SOURCE_RSS_FILE_PATH):
     logger.info("Source RSS cache not found")
     fetch_and_cache(SOURCE_RSS_URL, CACHE_SOURCE_RSS_FILE_PATH)
 else:
-    cache_source_rss_modification_timestamp = os.path.getmtime(CACHE_SOURCE_RSS_FILE_PATH)
-    cache_source_rss_age_seconds = current_timestamp - cache_source_rss_modification_timestamp
+    cache_source_rss_modification_timestamp = os.path.getmtime(
+        CACHE_SOURCE_RSS_FILE_PATH
+    )
+    cache_source_rss_age_seconds = (
+        current_timestamp - cache_source_rss_modification_timestamp
+    )
     cache_source_rss_age_minutes = math.floor(cache_source_rss_age_seconds / 60)
     logger.info(f"Source RSS cache is {cache_source_rss_age_minutes} minutes old")
 
@@ -133,7 +145,7 @@ except Exception as e:
     logger.error(f"Failed to parse {CACHE_SOURCE_RSS_FILE_PATH}:")
     logger.error(e)
     logger.error("\nContents of file:")
-    with open(CACHE_SOURCE_RSS_FILE_PATH, encoding='utf-8') as f:
+    with open(CACHE_SOURCE_RSS_FILE_PATH, encoding="utf-8") as f:
         logger.error(f.read())
     report_failure_and_exit()
 
@@ -149,25 +161,31 @@ new_root_element.extend(original_root_element)
 new_rss_tree = ElementTree(new_root_element)
 
 link_self = Element("{http://www.w3.org/2005/Atom}link")
-link_self.set("href", "https://phoronix.retromultiplayer.com/phoronix-rss-augmented.xml")
+link_self.set(
+    "href", "https://phoronix.retromultiplayer.com/phoronix-rss-augmented.xml"
+)
 link_self.set("rel", "self")
 link_self.set("type", "application/rss+xml")
 new_rss_tree.find("channel").insert(0, link_self)
 
-for item in new_rss_tree.iter('item'):
-    item_url = item.find('link').text
-    item_url_hash = hashlib.md5(item_url.encode('utf-8')).hexdigest()
+for item in new_rss_tree.iter("item"):
+    item_url = item.find("link").text
+    item_url_hash = hashlib.md5(item_url.encode("utf-8")).hexdigest()
     item_url_relative = item_url.removeprefix(WEBSITE_ROOT_URL)
-    item_cache_file_name = f'item_{item_url_hash}.html'
-    item_cache_file_path = CACHE_SOURCE_RSS_FILE_PATH = os.path.join(CACHE_ROOT, item_cache_file_name)
-    logger.info(f"---\nURL: {item_url_relative.ljust(40)} cache file name: {item_cache_file_name}")
+    item_cache_file_name = f"item_{item_url_hash}.html"
+    item_cache_file_path = CACHE_SOURCE_RSS_FILE_PATH = os.path.join(
+        CACHE_ROOT, item_cache_file_name
+    )
+    logger.info(
+        f"---\nURL: {item_url_relative.ljust(40)} cache file name: {item_cache_file_name}"
+    )
 
     # Check for item HTML cache, [re]download if necessary
     soup = None
     if not os.path.isfile(item_cache_file_path):
         logger.info(f"{item_url} cache not found")
         html_contents = fetch_and_cache(item_url, item_cache_file_path)
-        soup = BeautifulSoup(html_contents, 'html.parser')
+        soup = BeautifulSoup(html_contents, "html.parser")
     else:
         cache_item_modification_timestamp = os.path.getmtime(item_cache_file_path)
         cache_item_age_seconds = current_timestamp - cache_item_modification_timestamp
@@ -176,25 +194,25 @@ for item in new_rss_tree.iter('item'):
 
         if cache_item_age_hours < CACHE_ITEM_TTL:
             logger.info(f"Reusing cached {item_url}...")
-            with open(item_cache_file_path, encoding='utf-8') as f:
-                soup = BeautifulSoup(f, 'html.parser')
+            with open(item_cache_file_path, encoding="utf-8") as f:
+                soup = BeautifulSoup(f, "html.parser")
         else:
             html_contents = fetch_and_cache(item_url, item_cache_file_path)
-            soup = BeautifulSoup(html_contents, 'html.parser')
+            soup = BeautifulSoup(html_contents, "html.parser")
 
     # Extract article
-    article_html = soup.find('article')
+    article_html = soup.find("article")
 
     # Delete JavaScript
-    for script_tag in article_html.find_all('script'):
+    for script_tag in article_html.find_all("script"):
         script_tag.extract()
 
     # Delete sharebar
-    for sharebar in article_html.find_all('div', {"id": "sharebar"}):
+    for sharebar in article_html.find_all("div", {"id": "sharebar"}):
         sharebar.extract()
 
     # Delete <ins class="adsbygoogle"> RSS validator is complaining about
-    for ins_tag in article_html.find_all('ins', {"class": "adsbygoogle"}):
+    for ins_tag in article_html.find_all("ins", {"class": "adsbygoogle"}):
         ins_tag.extract()
 
     # Multipage articles contain page selector element
@@ -202,14 +220,16 @@ for item in new_rss_tree.iter('item'):
     # Delete it for now to pass validation
     # but maybe later i could implement
     # fetching the entire content of multipage articles.
-    for page_selector in article_html.find_all('select', {"id": "phx_article_page_selector"}):
+    for page_selector in article_html.find_all(
+        "select", {"id": "phx_article_page_selector"}
+    ):
         page_selector.extract()
 
     # Delete <h1> and <div class="author"> elements
     # because readers like Feedly provide their own
     # based on RSS metadata
-    article_html.find('h1').extract()
-    article_html.find('div', {'class': 'author'}).extract()
+    article_html.find("h1").extract()
+    article_html.find("div", {"class": "author"}).extract()
 
     # Some category images are way too big,
     # and Feedly ignores size tags set for these images
@@ -219,23 +239,29 @@ for item in new_rss_tree.iter('item'):
     # I could not find a way to limit image size in px/pt/%
     # that would work in Feedly web UI,
     # so replace category image tag with its alt value.
-    category_img_tag_container = article_html.find('div', {'class': 'content'}).find('div')
+    category_img_tag_container = article_html.find("div", {"class": "content"}).find(
+        "div"
+    )
     if category_img_tag_container:
-        category_img_tag = category_img_tag_container.select_one('img[src^="/assets/categories/"]')
+        category_img_tag = category_img_tag_container.select_one(
+            'img[src^="/assets/categories/"]'
+        )
         if category_img_tag:
             category_replacement_tag = soup.new_tag("div")
-            category_replacement_tag.string = category_img_tag['alt']
+            category_replacement_tag.string = category_img_tag["alt"]
             category_img_tag.replace_with(category_replacement_tag)
 
     # Fix relative links RSS validator is complaining about
     for relative_a_element in article_html.select('a[href^="/"]:not([href^="//"])'):
-        relative_a_element['href'] = f"{WEBSITE_ROOT_URL}{relative_a_element.get('href')}"
+        relative_a_element["href"] = (
+            f"{WEBSITE_ROOT_URL}{relative_a_element.get('href')}"
+        )
 
     # _Then_, fix a and img tags missing https:// protocol declaration
     for relative_a_element in article_html.select('a[href^="//"]'):
-        relative_a_element['href'] = f"https:{relative_a_element.get('href')}"
+        relative_a_element["href"] = f"https:{relative_a_element.get('href')}"
     for relative_img_element in article_html.select('img[src^="//"]'):
-        relative_img_element['src'] = f"https:{relative_img_element.get('src')}"
+        relative_img_element["src"] = f"https:{relative_img_element.get('src')}"
 
     # Comment counter is almost always wrong,
     # replace its text with a more honest one.
@@ -244,17 +270,19 @@ for item in new_rss_tree.iter('item'):
     # look like this:
     # <a href="https://www.phoronix.com/forums/node/1551155">Add A Comment</a>
     # <a href="https://www.phoronix.com/forums/node/1551633">9 Comments</a>
-    comments_a_element = article_html.find(href=re.compile('/forums/node/'), string=re.compile('Comment[s]?$'))
+    comments_a_element = article_html.find(
+        href=re.compile("/forums/node/"), string=re.compile("Comment[s]?$")
+    )
     if comments_a_element:
         comments_a_element.string = "[Comments]"
 
     # Replace <description> tag value with full content of the article
-    description = item.find('description')
+    description = item.find("description")
     description.text = CDATA(str(article_html))
 logger.info("---")
 
 # Output augmented RSS file
-new_rss_tree.write(OUTPUT_RSS_FILE_PATH, encoding = 'utf-8', xml_declaration = True)
+new_rss_tree.write(OUTPUT_RSS_FILE_PATH, encoding="utf-8", xml_declaration=True)
 
 # Clean up old item cache files
 current_time = time.time()
@@ -264,10 +292,14 @@ for item_cache_file_path in glob(os.path.join(CACHE_ROOT, "item_*.html")):
     cache_item_age_hours = cache_item_age_seconds / 60 / 60
 
     if cache_item_age_hours > CACHE_ITEM_TTL:
-        logger.info(f"Item cache file {os.path.basename(item_cache_file_path)} is >{math.floor(cache_item_age_hours)} hours old, deleting")
+        logger.info(
+            f"Item cache file {os.path.basename(item_cache_file_path)} is >{math.floor(cache_item_age_hours)} hours old, deleting"
+        )
         os.remove(item_cache_file_path)
 
-humanized_execution_duration = humanize.precisedelta(time.time() - current_timestamp, minimum_unit="seconds", format="%.0f")
+humanized_execution_duration = humanize.precisedelta(
+    time.time() - current_timestamp, minimum_unit="seconds", format="%.0f"
+)
 logger.info(f"Completed in {humanized_execution_duration}")
 
 # Report success to Better Stack
